f58d3dc7643fc18b5d1041e9f5939fcc15214f84
kevin.wang <kwin.wang@gmail.com>
2025-04-18 10:01:09 +0800

    test: improve shared memory test for cross-platform compatibility

--- src/TestUnits/test_shm.cpp
+++ src/TestUnits/test_shm.cpp
@@ -1,9 +1,20 @@
 ﻿#include "gtest/gtest/gtest.h"
 #include "../WtShareHelper/WtShareHelper.h"
+#include "../Share/StdUtils.hpp"
+#include "../Share/StrUtil.hpp"
+#include <boost/filesystem.hpp>
 
 TEST(test_shm, test_sharehelper)
 {
-	EXPECT_TRUE(init_slave("uft", "E:\\deploy_uft\\uft_test\\.share"));
+#ifdef _WIN32
+	std::string base_path = "E:\\deploy_uft\\uft_test\\.share";
+#else
+	std::string base_path = boost::filesystem::temp_directory_path().string() + "/uft_test.shm";
+#endif
+	std::string shm_path = StrUtil::standardisePath(base_path);
+
+	// First initialize as master to set up the shared memory
+	EXPECT_TRUE(init_master("uft", shm_path.c_str()));
 
 	//EXPECT_TRUE(set_string("test", "section", "string", "value"));
 	//EXPECT_TRUE(set_string("test", "section", "string", "value2"));
@@ -18,9 +29,19 @@ TEST(test_shm, test_sharehelper)
 	//EXPECT_TRUE(set_double("test", "section", "double", DBL_MAX));
 	//EXPECT_TRUE(set_double("test", "section", "double", FLT_MAX));
 
+	// Set initial values
+	EXPECT_TRUE(set_int32("uft", "uft_demo", "offset", 1));
+	EXPECT_TRUE(set_uint32("uft", "uft_demo", "second", 10));
+	EXPECT_TRUE(set_double("uft", "uft_demo", "lots", 1.0));
 
+	// Now initialize as slave to read the values
+	EXPECT_TRUE(init_slave("uft", shm_path.c_str()));
+
+	// Verify the values
 	EXPECT_EQ(get_int32("uft", "uft_demo", "offset"), 1);
-	EXPECT_EQ(get_uint32("uft", "uft_demo", "freq"), 0);
 	EXPECT_EQ(get_uint32("uft", "uft_demo", "second"), 10);
 	EXPECT_EQ(get_double("uft", "uft_demo", "lots"), 1.0);
+
+	// Clean up
+	release_slave("uft");
 }
